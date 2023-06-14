87a7427e06569b37a39a9aee16d622dd5e041537
Adam Alton <270255+adamalton@users.noreply.github.com>
2023-06-14 20:31:22 +0100

    Django admin improvements

--- massmigration/admin.py
+++ massmigration/admin.py
@@ -1,23 +1,22 @@
 # Third party
 from django.contrib import admin
 
-# Djangae Migrations
+# Mass Migration
+from .loader import store
 from .models import MigrationRecord
-
-
-class AppLabelFilter():
-    # TODO: make a filter for the `MigrationRecord.app_label` field, but rather than getting the
-    # possible values from the DB, provide them as choices from django.apps.get_apps() or whatever
-    # that thing is. Although maybe that's not necessary? Can we just set `choices` on the
-    # Migration.app_label field?
-    pass
 
 
 class MigrationRecordAdmin(admin.ModelAdmin):
     """ Custom admin class for the Migration model. """
 
     ordering = ("-initiated_at",)
-    list_display = ("name", "initiated_at", "is_applied", "applied_at")
+    list_display = ("key", "initiated_at", "is_applied", "applied_at", "was_faked")
+    list_filter = ("app_label", "is_applied", "was_faked")
+    readonly_fields = ("app_label", "name", "description")
+
+    def description(self, obj):
+        migration = store.by_key[obj.key]
+        return migration.description
 
 
 admin.site.register(MigrationRecord, MigrationRecordAdmin)
