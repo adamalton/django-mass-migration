9cea10c32a40d5f1122875d00c28e6469d732e99
Adam Alton <270255+adamalton@users.noreply.github.com>
2023-07-05 12:11:08 +0100

    Add "success" field into list display in Django admin

--- massmigration/admin.py
+++ massmigration/admin.py
@@ -1,5 +1,6 @@
 # Third party
 from django.contrib import admin
+from django.contrib.admin.templatetags.admin_list import _boolean_icon
 
 # Mass Migration
 from .loader import store
@@ -10,13 +11,16 @@ class MigrationRecordAdmin(admin.ModelAdmin):
     """ Custom admin class for the Migration model. """
 
     ordering = ("-initiated_at",)
-    list_display = ("key", "initiated_at", "is_applied", "applied_at", "was_faked")
+    list_display = ("key", "initiated_at", "is_applied", "successful", "applied_at", "was_faked")
     list_filter = ("app_label", "is_applied", "was_faked")
     readonly_fields = ("app_label", "name", "description")
 
     def description(self, obj):
         migration = store.by_key[obj.key]
         return migration.description
+
+    def successful(self, obj):
+        return _boolean_icon((not obj.has_error) if obj.is_applied else None)
 
 
 admin.site.register(MigrationRecord, MigrationRecordAdmin)
