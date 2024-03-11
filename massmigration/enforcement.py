720efbbd0edc461ad19f5412854966499c62ac87
Paolo Chillari <paolo@potatolondon.com>
2024-03-11 16:40:30 +0100

    Fix issues with enforcement

--- massmigration/enforcement.py
+++ massmigration/enforcement.py
@@ -22,66 +22,74 @@ APPLIED_MIGRATIONS_CACHE = {}
 
 
 def get_migration_key(migration_id_str_or_tuple):
-    if is_valid_migration_id(migration_id_str_or_tuple):
-        return migration_id_str_or_tuple
+    if isinstance(migration_id_str_or_tuple, (list, tuple)):
+        key = MigrationRecord.key_from_name_tuple(migration_id_str_or_tuple)
     else:
-        return MigrationRecord.key_from_name_tuple(migration_id_str_or_tuple)
+        key = migration_id_str_or_tuple
+
+    if store.by_key.get(key, None) is None:
+        raise ValueError(
+            f"Provided migration with key '{key}' not found."
+            f"Available keys are {', '.join(store.by_key.keys())}"
+        )
+
+    return key
 
 
 def migration_is_applied(migration_identifier, db_alias):
     """ Tells you whether or not the specified migration has been applied to the DB.
         Positive (True) responses are cached to avoid repeated DB queries.
     """
-    key = (get_migration_key(migration_identifier), db_alias)
+    migration_key = get_migration_key(migration_identifier)
+    cache_key = (get_migration_key(migration_identifier), db_alias)
     try:
-        return APPLIED_MIGRATIONS_CACHE[key]
+        return APPLIED_MIGRATIONS_CACHE[cache_key]
     except KeyError:
-        applied = MigrationRecord.objects.using(db_alias).filter(key=key, is_applied=True).exists()
+        applied = MigrationRecord.objects.using(db_alias).filter(key=migration_key, is_applied=True).exists()
         if applied:
-            APPLIED_MIGRATIONS_CACHE[key] = True
+            APPLIED_MIGRATIONS_CACHE[cache_key] = True
             return True
     return False
 
 
-def requires_migration(migration_identifier, db_alias=[], is_view=False, skip_in_tests=True):
+def requires_migration(migration_identifier, db_aliases=[], is_view=False, skip_in_tests=True):
     """ Function decorator which prevents the function being run if the specified migration is not
         applied.
     """
-    def enforce_migration():
-        key = get_migration_key(migration_identifier)
-
-        migration = store.by_key(key)
-        if not migration:
-            raise ValueError(f"Migration with key '{key}' not found.")
-
-        allowed_db_aliases = migration.get_allowed_db_aliases()
-
-        # By default if db_alias is not specified, we assume the migration needs to be applied on all the allowed_db_aliases
-        # specified in the migration
-        if not db_alias:
-            required_migrations_db_aliases = allowed_db_aliases
-        else:
-            if db_alias not in allowed_db_aliases:
-                raise DbAliasNotAllowed(
-                    f"Requires_migration improperly configured. "
-                    f"Configured to require migration <{migration_identifier}> on DB <{db_alias}> while the alled DBs are {', '.join(allowed_db_aliases)}."
-                )
-            else:
-                required_migrations_db_aliases = db_alias
-
-        if not (skip_in_tests or in_tests()):
-            if not all([
-                migration_is_applied(migration_identifier, db_alias)
-                for db_alias in required_migrations_db_aliases
-            ]):
-                raise RequiredMigrationNotApplied(
-                    f"Function '{function}'' requires migration {migration_identifier} which has "
-                    "not been applied."
-                )
 
     def decorator(function):
         @wraps(function)
         def replacement(*args, **kwargs):
+            def enforce_migration():
+                key = get_migration_key(migration_identifier)
+
+                migration = store.by_key.get(key, None)
+                if not migration:
+                    raise ValueError(f"Migration with key '{key}' not found.")
+
+                allowed_db_aliases = migration.get_allowed_db_aliases()
+
+                # By default if db_alias is not specified, we assume the migration needs to be applied on all the allowed_db_aliases
+                # specified in the migration
+                if not db_aliases:
+                    required_migrations_db_aliases = allowed_db_aliases
+                else:
+                    if any([db_alias not in allowed_db_aliases for db_alias in db_aliases]):
+                        raise DbAliasNotAllowed(
+                            f"requires_migration decorator improperly configured. "
+                            f"It requires the migration <{migration_identifier}> to have run on <{', '.join(db_aliases)}> "
+                            f"while the allowes databases are <{', '.join(allowed_db_aliases)}>."
+                        )
+                    else:
+                        required_migrations_db_aliases = db_aliases
+
+                if not (skip_in_tests and in_tests()):
+                    if not all([migration_is_applied(migration_identifier, db_alias) for db_alias in required_migrations_db_aliases]):
+                        raise RequiredMigrationNotApplied(
+                            f"Migration '{function}'' requires migration {migration_identifier} which has "
+                            "not been applied."
+                        )
+
             if is_view:
                 try:
                     enforce_migration()
@@ -102,8 +110,8 @@ def requires_migration(migration_identifier, db_alias=[], is_view=False, skip_in
     return decorator
 
 
-def view_requires_migration(migration_identifier, skip_in_tests=True):
+def view_requires_migration(migration_identifier, db_aliases=[], skip_in_tests=True):
     """ Same as `requires_migration`, but for view functions. Returns a 503 status HttpResponse
         rather than raising an exception.
     """
-    return requires_migration(migration_identifier, is_view=True, skip_in_tests=skip_in_tests)
+    return requires_migration(migration_identifier, is_view=True, db_aliases=db_aliases, skip_in_tests=skip_in_tests)
