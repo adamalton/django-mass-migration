5f9ec85a4cfa30c3a03787c6aa9e06de7b97a43e
Adam Alton <270255+adamalton@users.noreply.github.com>
2024-06-03 17:00:33 +0100

    Fix `get_transaction` to not return gcloudc.db.transaction for a Firestore connection

--- massmigration/utils/transaction.py
+++ massmigration/utils/transaction.py
@@ -1,21 +1,12 @@
 # Third party
 from django.conf import settings
-from django.db import router, transaction as django_transaction
-
-# Mass Migration
-from massmigration.models import MigrationRecord
-
-
-# TODO: if we want to add true multi-database support (i.e. allow migrations on two different
-# databases within the same project) then we should probably add a `connection_name` attribute to
-# the Migration class and pass that into both here and into .using() calls in various places.
-# That said, should you be allowed to run the same migration on two different databases?
+from django.db import transaction as django_transaction
 
 
 def get_transaction(db_alias):
     engine = settings.DATABASES[db_alias]["ENGINE"]
     transaction = django_transaction
-    if engine.startswith("gcloudc.db.backends"):
+    if engine == "gcloudc.db.backends.datastore":
         try:
             from gcloudc.db import transaction as gcloudc_transaction
             transaction = gcloudc_transaction
